dd59123423daffebd9f80df3f8858de50a2ebe8f
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2024-02-25 14:29:24 -0500

    Compute node check, some translation

--- scripts/crunch.py
+++ scripts/crunch.py
@@ -5,26 +5,26 @@ import timeit
 
 
 def crunch(lib, N):
-    '''Calcul matriciel "lourd"
+    '''Stress test with expensive matrix operations
     Arguments :
-        lib - numpy ou cupy
-        N - taille de la matrice cubique à créer
+        lib - numpy or cupy library
+        N - size of the cubic matrix to create
     '''
-    matrice = lib.ones((N, N, N))
-    matrice /= (N + 1.0)
-    matrice.cumsum()
-    matrice @ matrice  # O(N^3)
+    matrix_3d = lib.ones((N, N, N))
+    matrix_3d /= (N + 1.0)
+    matrix_3d.cumsum()
+    matrix_3d @ matrix_3d
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-n', '--taille', type=int, default=512, help='Taille matrice 3D')
-    parser.add_argument('--cpu', action='store_true', help='Calcul sur CPU')
-    parser.add_argument('--gpu', action='store_true', help='Calcul sur GPU')
+        '-n', '--size', type=int, default=512, help='Size of 3D matrix')
+    parser.add_argument('--cpu', action='store_true', help='Use CPU cores')
+    parser.add_argument('--gpu', action='store_true', help='Use one GPU')
 
     args = parser.parse_args()
-    n = args.taille
+    n = args.size
 
     if args.cpu:
         import numpy as np
--- scripts/crunch.sh
+++ scripts/crunch.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-SYNTAXE="Syntaxe: bash $0 --cpu|--gpu [1|4]"
+if [ -z "$SLURM_TMPDIR" ]; then
+  echo 'Error: this script must be used from a compute node.'
+  echo 'You must first run the "salloc" command.'
+  exit
+fi
+
+SYNTAX="Syntax: bash $0 --cpu|--gpu [1|4]"
 
 if [ "$#" -lt 1 ]; then
-  echo $SYNTAXE
+  echo $SYNTAX
   exit
 fi
 
 if [ "$1" = '--cpu' ]; then
   if ! [ "$2" = 1 ] && ! [ "$2" = 4 ]; then
-    echo Erreur: le second paramètre doit être 1 ou 4
-    echo $SYNTAXE
+    echo 'Error: the second argument must be 1 or 4'
+    echo $SYNTAX
     exit
   fi
 
@@ -22,7 +28,7 @@ if [ "$1" = '--cpu' ]; then
 
   export OMP_NUM_THREADS=$2
   time -p python $(dirname $0)/crunch.py -n 672 --cpu > t$2.log &
-  echo "C'est parti!"
+  echo "It is running!"
 
 elif  [ "$1" = '--gpu' ]; then
   module load gcc/9.3.0 cuda/11.4 python/3.8.10 &> /dev/null
@@ -32,9 +38,9 @@ elif  [ "$1" = '--gpu' ]; then
   pip install --no-index numpy==1.24.2 cupy==11.2.0
 
   time -p python $(dirname $0)/crunch.py -n 672 --gpu > tg.log &
-  echo "C'est parti!"
+  echo "It is running!"
 
 else
-  echo Erreur: le premier paramètre doit être --cpu ou --gpu
-  echo $SYNTAXE
+  echo 'Error: the first argument must be --cpu or --gpu'
+  echo $SYNTAX
 fi
